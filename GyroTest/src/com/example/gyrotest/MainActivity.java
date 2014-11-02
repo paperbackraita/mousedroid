c6cb33df2936a6d81fb0e594d5024e8f693911f1
prateeklohchubh <prateeksinghlohchubh@gmail.com>
2014-11-03 01:51:04 +0530

    Reduced Noise in gyro reading

--- GyroTest/src/com/example/gyrotest/MainActivity.java
+++ GyroTest/src/com/example/gyrotest/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.gyrotest;
 
+
+import java.util.*;
 import android.app.Activity;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
@@ -13,8 +15,11 @@ import android.widget.TextView;
 
 public class MainActivity extends Activity implements SensorEventListener {
 	private TextView tv;
-	private SensorManager sManager;
+	private SensorManager sManager;	
 	private Sensor gyroscope;
+	float[] rolls = new float[3];
+	
+	
 	
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,7 +27,7 @@ public class MainActivity extends Activity implements SensorEventListener {
         setContentView(R.layout.activity_main);
         tv = (TextView) findViewById(R.id.tv);
         sManager = (SensorManager) getSystemService(SENSOR_SERVICE);
-        gyroscope = sManager.getDefaultSensor(Sensor.TYPE_ORIENTATION);
+        gyroscope = sManager.getDefaultSensor(Sensor.TYPE_ORIENTATION); 
         
     }
     
@@ -44,20 +49,32 @@ public class MainActivity extends Activity implements SensorEventListener {
     @Override  
     public void onAccuracyChanged(Sensor arg0, int arg1)  
     {  
-    }  
-  
+    } 
+   
     @Override  
     public void onSensorChanged(SensorEvent event)  
     {  
         if (event.sensor.getType() == Sensor.TYPE_ORIENTATION)
-    	{if (event.accuracy == SensorManager.SENSOR_STATUS_UNRELIABLE)  
-        {  
-            return;  
-        }  
-  
-        tv.setText("Orientation X :"+ Float.toString(event.values[2]) +"\n"+  
-                   "Orientation Y :"+ Float.toString(event.values[1]) +"\n"+  
-                   "Orientation Z :"+ Float.toString(event.values[0]));
+    	{
+        	if (event.accuracy == SensorManager.SENSOR_STATUS_UNRELIABLE)  
+        	{  
+        		return;  
+        	}  
+        	else{  
+        		if(Math.abs(Math.abs(rolls[0])-Math.abs(event.values[2])) > 2.5)
+        			rolls[0]=event.values[2];
+        		if(Math.abs(Math.abs(rolls[1])-Math.abs(event.values[1])) > 2.5)
+        			rolls[1]=event.values[1];
+        		if(Math.abs(Math.abs(rolls[2])-Math.abs(event.values[0])) > 2)
+        			rolls[2]=event.values[0];
+        		tv.setText("Orientation X :"+ Float.toString(rolls[0]) +"\n"+  
+        				"Orientation Y :"+ Float.toString(rolls[1]) +"\n"+  
+        				"Orientation Z :"+ Float.toString(rolls[2]));
+        		
+        		/*tv.setText("Orientation X :"+ Float.toString(event.values[2]) +"\n"+  
+        				"Orientation Y :"+ Float.toString(event.values[1]) +"\n"+  
+        				"Orientation Z :"+ Float.toString(event.values[0]));*/
+        	}
     	}
     }  
 
